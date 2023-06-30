133aa1bc2256734ff875826e5c42031e08a6871f
Joey <106037759+techaddict0x@users.noreply.github.com>
2023-06-30 17:26:32 +0200

    Add files via upload

---
+++ _next/static/xRvkW28hn8l2Ia37T2kBz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-ecaca653dda3f71b.js","static/chunks/pages/index-f36b06c5cdcf90b5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
